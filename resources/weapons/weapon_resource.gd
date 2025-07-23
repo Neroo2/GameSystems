b3adb849b1105823eb2d625db253c20a831fe8af
Nero <nelsonnnnn.junior@gmail.com>
2025-07-22 22:18:58 -0300

    Shoot System / Flickering lights
    Now weapons shoot based on the distance;
    New function to add a weapon to the playe.

---
+++ resources/weapons/weapon_resource.gd
@@ -0,0 +1,5 @@
+extends Resource
+class_name Weapon
+
+@export var weaponName: String
+@export var weaponScene: PackedScene
--- scripts/weapons/weapon_manager.gd
+++ scripts/weapons/weapon_manager.gd
@@ -3,25 +3,48 @@ extends Node3D
 @export var weaponPoints: Node3D
 @export var detectionArea: Area3D
 
+@export var weapon: Weapon
+
+
 var target_enemy = null
-
-func _ready() -> void:
-	pass
-
 
 
 func _physics_process(delta: float) -> void:
-	detect_enemy()
-	if target_enemy != null:
-		for point in weaponPoints.get_children():
-			point.look_at(target_enemy.global_position)
+	var enemies_in_area = get_enemies_in_area()
+	
+	for point in weaponPoints.get_children():
+		var closest_enemy = get_closest_enemy_to_point(point, enemies_in_area)
+		if closest_enemy:
+			point.look_at(closest_enemy.global_position)
 			
 			
 
+func add_weapon_to_player(weapon: Weapon):
 
-func detect_enemy():
-	var bodiesInArea = detectionArea.get_overlapping_bodies()
-	for entity in bodiesInArea:
+	for point in weaponPoints.get_children():
+		if point.get_child_count() == 0:
+			var weaponScene = weapon.weaponScene.instantiate()
+			point.add_child(weaponScene)
+			return
+
+func get_enemies_in_area() -> Array:
+	var enemies: Array = []
+	for entity in detectionArea.get_overlapping_bodies():
 		if entity.is_in_group("Enemy"):
-			target_enemy = bodiesInArea.front()
+			enemies.append(entity)
+	return enemies
+
+
+func get_closest_enemy_to_point(point: Node3D, enemies: Array) -> Node3D:
+	var closest_enemy = null
+	var closest_distance = INF
+
+	for enemy in enemies:
+		var distance = point.global_position.distance_to(enemy.global_position)
+		if distance < closest_distance:
+			closest_distance = distance
+			closest_enemy = enemy
+	
+	return closest_enemy
+
 		
