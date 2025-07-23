5af9482d8d2faa9d1a051894c8a8aa3a742ddf94
Nero <nelsonnnnn.junior@gmail.com>
2025-07-23 19:04:38 -0300

    Enemy spawn system removed

--- scripts/entities/enemies/enemy_spawner.gd
+++ scripts/entities/enemies/enemy_spawner.gd
@@ -1,25 +1,15 @@
+@tool
 extends Area3D
 
 
 const ENEMY = preload("res://scenes/entities/enemies/enemy.tscn")
+@export var collisionShape : CollisionShape3D
 
-
-func _physics_process(delta: float) -> void:
-	print($CollisionShape3D.shape.extents)
-	
 
 
 func spawn_enemy():
-	var box_shape = $CollisionShape3D.shape as BoxShape3D
-	var extents = box_shape.extents
 
-	var random_offset = Vector3(
-		randf_range(-extents.x, extents.x),
-		randf_range(-extents.y, extents.y),
-		randf_range(-extents.z, extents.z)
-	)
-
-	var spawn_pos = global_transform.origin + random_offset
+	var spawn_pos = $".".global_position
 	var enemy_instance = ENEMY.instantiate()
 
 	get_tree().current_scene.add_child(enemy_instance)
---
+++ scripts/general/test_map.gd
@@ -0,0 +1,8 @@
+extends Node3D
+
+
+const ENEMY = preload("res://scenes/entities/enemies/enemy.tscn")
+
+
+func _physics_process(delta: float) -> void:
+	
